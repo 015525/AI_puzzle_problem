9ee099a670c96be8c549f1f401fe2399b6bf38a4
Mahmoud Gad <99031876+mega404@users.noreply.github.com>
2022-10-18 22:44:45 +0200

    Delete astar directory

--- astar/test.py
+++
@@ -1,9 +0,0 @@
-from astar.solver import solver
-from astar.heuristic.heuristic import manhattan
-from astar.heuristic.heuristic import euclidian
-
-if __name__ == "__main__":
-    a = solver()
-    a.solve(123456780, manhattan())
-    a.solve(123456780, euclidian())
-
